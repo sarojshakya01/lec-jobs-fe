59c38928b8e863b49412442e98846e5af4b50116
Saroj Shakya <saroj@Sarojs-MacBook-Pro-2.local>
2023-07-31 19:59:15 +0545

    login page enabled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Component } from "react";
 import "./App.css";
 import Home from "./components/Home";
-// import Login from './components/Login';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
-      // <Login/>
-      <Home />
+      <Login/>
+      // <Home />
     );
   }
 }
