5cd926b5deffc03c35ed2415fa9b2d5212334171
Saroj Shakya <saroj@Sarojs-MacBook-Pro-2.local>
2023-08-12 09:11:16 +0545

    added user session

--- src/App.js
+++ src/App.js
@@ -15,11 +15,23 @@ class App extends Component {
 
   loginUser(user) {
     this.setState({ userLoggedIn: true, user: user });
+    window.sessionStorage.setItem("loggedInUser", JSON.stringify(user));
+    window.sessionStorage.setItem("isLoggedIn", "true");
   }
 
   render() {
-    if (this.state.userLoggedIn) {
-      return <Home user={this.state.user} />;
+    let user = this.state.user;
+    let userLoggedIn = this.state.userLoggedIn;
+    if (!Object.keys(user).length || !userLoggedIn) {
+      user = window.sessionStorage.getItem("loggedInUser");
+      userLoggedIn = window.sessionStorage.getItem("isLoggedIn") === "true";
+      if (userLoggedIn) {
+        user = JSON.parse(user);
+      }
+    }
+
+    if (userLoggedIn) {
+      return <Home user={user} />;
     } else {
       return <Login loginUser={this.loginUser} />;
     }
