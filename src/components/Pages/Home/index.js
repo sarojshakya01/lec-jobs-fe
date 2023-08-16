ee33df937c913af57fa21b94b537b03ffa60edf1
Saroj Shakya <saroj@Sarojs-MacBook-Pro-2.local>
2023-08-16 12:10:24 +0545

    removed commented part'

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -4,23 +4,7 @@ import ChatBox from "./ChatBox";
 import Main from "./Main";
 
 class Home extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     user: {},
-  //   };
-  // }
-  // componentDidMount() {
-  //   fetch("http://localhost:5001/api/v1/user")
-  //     .then((resp) => resp.json())
-  //     .then((data) => {
-  //       this.setState({ user: data });
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  // }
-
+ 
   render() {
     const user = this.props.user;
     // const user = { ...this.props.user };
